ccc672dbb27ce79c37bf45bbb0cd4bd21225a2ff
Sandip Gardi <sandip.gardi@think360.ai>
2025-02-19 15:39:19 +0530

    Git deploy

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,7 +67,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
-
+//delete
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
