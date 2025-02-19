752e75b09388df977bcac2c0c1bebe65d40a8d80
Sandip Gardi <sandip.gardi@think360.ai>
2025-02-19 16:39:07 +0530

    Almost done

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -110,7 +110,7 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
                       <UpdateInvoice id={invoice.id} />
-                      {/* <DeleteInvoice id={invoice.id} /> */}
+                      <DeleteInvoice id={invoice.id} />
                     </div>
                   </td>
                 </tr>
