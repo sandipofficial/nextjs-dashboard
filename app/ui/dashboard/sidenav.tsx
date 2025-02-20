61c8d6cca9b6aee997abe509673adfd2bcf7c6ac
Sandip Gardi <sandip.gardi@think360.ai>
2025-02-20 16:05:52 +0530

    SOLVED: Linting issues

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,6 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
-import { redirect } from 'next/dist/server/api-utils';
 
 export default function SideNav() {
   return (
